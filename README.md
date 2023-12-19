336224b7b009a754eb032de304d5764181f81898
JSaucourt <jysru@pm.me>
2023-12-19 10:16:04 +0100

    Added base files

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# waveoptics_tools
-Some tools regarding wave optics
+# Waveoptics tools
+
+
+## Installation
+
+Use in virtual environment or you own environment:
+
+```bash
+$ python -m pip install -r requirements.txt
+$ pip install -e .
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="waveoptics-tools",
+    version="0.1",
+    author="Jysru",
+    author_email="jysru@pm.me",
+    description="Some tools for wave optics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jysru/waveoptics_tools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
